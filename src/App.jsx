8843b607368bf5135b3fe74e052d73cae166da5f
Sander Sims <114922010+sandersims@users.noreply.github.com>
2025-04-05 16:32:31 +0300

    added S3 and lambda

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ export default function App() {
       width="70%"
       margin="0 auto"
     >
-      <Heading level={1}>My Profile</Heading>
+      <Heading level={1}>Ametnike palgaandmed</Heading>
 
       <Divider />
       <DataTable data={officials} />
